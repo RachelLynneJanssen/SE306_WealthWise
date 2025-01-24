6bba777724c699e04102539431de0b219469ada4
RachelLynneJanssen <rachel.l.janssen@jacks.sdstate.edu>
2025-01-23 22:16:41 -0600

    Blank pages creaed for all nav bar links (except news)

---
+++ WealthWise RCD/Controllers/FinancialCalcsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WealthWise_RCD.Controllers
+{
+    public class FinancialCalcsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
