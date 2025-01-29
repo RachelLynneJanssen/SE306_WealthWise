4bd02ea19765bf70c74d44be449a3b69120acc0f
LilliAP <lillipmhsap@gmail.com>
2025-01-29 11:37:14 -0600

    Updated connection string

---
+++ WealthWise RCD/Migrations/20250129172740_testConnection.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace WealthWise_RCD.Migrations
+{
+    /// <inheritdoc />
+    public partial class testConnection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "testtests");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "testtests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_testtests", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+    }
+}
