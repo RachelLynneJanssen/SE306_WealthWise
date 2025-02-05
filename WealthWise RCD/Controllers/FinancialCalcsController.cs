68ad0abf01ffd6c8636bbf40668967f01dda0d5e
Max <58192471+DiMister@users.noreply.github.com>
2025-02-05 12:49:49 -0600

    interest calculator

--- WealthWise RCD/Controllers/FinancialCalcsController.cs
+++ WealthWise RCD/Controllers/FinancialCalcsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WealthWise_RCD.Models;
 
 namespace WealthWise_RCD.Controllers
 {
@@ -6,6 +7,30 @@ namespace WealthWise_RCD.Controllers
     {
         public IActionResult Index()
         {
+            if (Request.Method == "POST")
+            {
+                string loanAmountStr = String.Format("{0}", Request.Form["loanAmount"]);
+                string interestRateStr = String.Format("{0}", Request.Form["interestRate"]);
+                string numberOfPaymentsStr = String.Format("{0}", Request.Form["numberOfPayments"]);
+                string timeStr = String.Format("{0}", Request.Form["time"]);
+
+                if (double.TryParse(loanAmountStr, out double loanAmount) &&
+                    double.TryParse(interestRateStr, out double interestRate) &&
+                    double.TryParse(numberOfPaymentsStr, out double numberOfPayments) &&
+                    double.TryParse(timeStr, out double time))
+                {
+                    double amount = loanAmount * Math.Pow(1 + (interestRate/numberOfPayments), (time*numberOfPayments));
+                    System.Diagnostics.Debug.WriteLine(amount);
+                    ViewData["Amount"] = amount;
+                }
+                else
+                {
+                    ViewData["Amount"] = "Invalid input";
+                    System.Diagnostics.Debug.WriteLine("Invalid input");
+                }
+            }
+            
+
             return View();
         }
     }
