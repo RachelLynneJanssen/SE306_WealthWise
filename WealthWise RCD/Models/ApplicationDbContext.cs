395a349d2b97840ddfb96b09ee0889b81fd1efb9
LilliAP <lillipmhsap@gmail.com>
2025-02-10 20:32:48 -0600

    Updated DbContext

--- WealthWise RCD/Models/ApplicationDbContext.cs
+++ WealthWise RCD/Models/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace WealthWise_RCD.Models
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext() { }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
