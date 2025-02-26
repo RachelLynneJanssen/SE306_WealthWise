9bb30839f2370a8f6a0991469673465def79ac08
RachelLynneJanssen <rachel.l.janssen@jacks.sdstate.edu>
2025-02-25 19:38:54 -0600

    Learning hub stuff for Advisors and fixing navbar links for advisors to stay in advisor area.

--- WealthWise RCD/Areas/Advisor/Controllers/LearningHubController.cs
+++ WealthWise RCD/Areas/Advisor/Controllers/LearningHubController.cs
@@ -1,11 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WealthWise_RCD.Models.DatabaseModels;
 
 namespace WealthWise_RCD.Areas.Advisor.Controllers
 {
+    [Area("Advisor")]
     public class LearningHubController : Controller
     {
-        [Area("Advisor")]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult BlogPosts()
+        {
+            // To be removed later
+            var dummyBlogPosts = new List<Blog>
+            {
+                new Blog { Title = "Blog Post 1", Topic = "Topic", PublicationDate= DateTime.Now, Content = "This is the content of Blog Post 1." },
+                new Blog { Title = "Blog Post 2", Topic = "Topic", PublicationDate= DateTime.Now, Content = "This is the content of Blog Post 2." },
+                new Blog { Title = "Blog Post 3", Topic = "Topic", PublicationDate= DateTime.Now, Content = "This is the content of Blog Post 3." },
+            };
+
+            return View(dummyBlogPosts);
+        }
+
+        public IActionResult References()
+        {
+            return View();
+        }
+
+        public IActionResult TipsAndTricks()
         {
             return View();
         }
