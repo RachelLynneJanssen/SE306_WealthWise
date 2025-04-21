8296c3a02d6ed9b59cc1b5c308000050a65ee4d7
LilliAP <lillipmhsap@gmail.com>
2025-04-20 21:01:23 -0500

    Test stuff

--- WealthWise RCD/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ WealthWise RCD/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -25,7 +25,6 @@ namespace WealthWise_RCD.Areas.Identity.Pages.Account
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly ILogger<LoginModel> _logger;
-
         public LoginModel(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager, ILogger<LoginModel> logger)
         {
             _signInManager = signInManager;
@@ -56,32 +55,21 @@ namespace WealthWise_RCD.Areas.Identity.Pages.Account
             {
                 ModelState.AddModelError(string.Empty, ErrorMessage);
             }
-
             returnUrl ??= Url.Content("~/");
-
-            // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
-
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-
             ReturnUrl = returnUrl;
         }
-
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
             returnUrl ??= Url.Content("~/");
-
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-
             if (ModelState.IsValid)
             {
-                // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var result = await _signInManager.PasswordSignInAsync(Input.Username, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
-
                     var user = await _userManager.FindByNameAsync(Input.Username);
                     if (user != null)
                     {
@@ -89,7 +77,7 @@ namespace WealthWise_RCD.Areas.Identity.Pages.Account
                         {
                             return RedirectToAction("Index", "Home", new { area = "User" });
                         }
-                        else if (await _userManager.IsInRoleAsync(user, "Advisor"))
+                        else if (!await _userManager.IsInRoleAsync(user, "Advisor"))
                         {
                             return RedirectToAction("Index", "Home", new { area = "Advisor" });
                         }
@@ -98,27 +86,13 @@ namespace WealthWise_RCD.Areas.Identity.Pages.Account
                             return LocalRedirect(returnUrl);
                         }
                     }
-
                 }
-                /*NOT IMPLEMENTING 2FA
-                if (result.RequiresTwoFactor)
-                {
-                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
-                }
-                */
-                /*NOT IMPLEMENTING LOCKOUT
-                if (result.IsLockedOut)
-                {
-                    _logger.LogWarning("User account locked out.");
-                    return RedirectToPage("./Lockout");
-                }*/
                 else
                 {
                     ModelState.AddModelError(string.Empty, "Invalid login attempt.");
                     return Page();
                 }
             }
-            // If we got this far, something failed, redisplay form
             return Page();
         }
     }
