e7677e59316a932dcfae528e179c0aa9e33fe385
LilliAP <lillipmhsap@gmail.com>
2025-04-20 21:05:21 -0500

    Changed ChangePassword logic (user is actually logged out now)

--- WealthWise RCD/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
+++ WealthWise RCD/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -58,20 +58,11 @@ namespace WealthWise_RCD.Areas.Identity.Pages.Account.Manage
             [Display(Name = "Current password")]
             public string OldPassword { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 8)]
             [DataType(DataType.Password)]
             [Display(Name = "New password")]
             public string NewPassword { get; set; }
-
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
             [DataType(DataType.Password)]
             [Display(Name = "Confirm new password")]
             [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
@@ -121,7 +112,9 @@ namespace WealthWise_RCD.Areas.Identity.Pages.Account.Manage
             await _signInManager.RefreshSignInAsync(user);
             _logger.LogInformation("User changed their password successfully.");
             //StatusMessage = "Your password has been changed.";
-
+            await _signInManager.SignOutAsync();
+            Response.Cookies.Delete(".AspNetCore.Identity.Application"); // Ensure cookie is removed
+            _logger.LogInformation("User logged out.");
             return RedirectToPage("../Login");
         }
     }
